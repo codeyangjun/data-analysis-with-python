b189aae447fee5d0634d8b1d364c2394b4891e01
codeyangjun <codeyangjun@126.com>
2016-04-14 22:19:01 +0800

    add week1 project

---
+++ printName.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 13 21:33:32 2016
+
+@author: yangjun1
+"""
+surname = raw_input("Input your surname:")
+firstname = raw_input("input your firstname:")
+print "Your surname is:"
+print surname
+print "Your firstname is:"
+print firstname
+fullname = surname + ' ' + firstname
+print "Your full name is:"
+print fullname
+
